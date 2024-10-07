4fa7506c3c6cec42fc1ece5fb39cc1ae149d693b
qwdltjdals <iu_love@kakao.com>
2024-10-07 15:05:08 +0900

    관리자 페이지 기본 틀 css

---
+++ src/components/Admin/ProductDelete/ProductDelete.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductDelete(props) {
+    return (
+        <div>
+            <h1>상품 삭제 페이지 입니다</h1>
+        </div>
+    );
+}
+
+export default ProductDelete;
---
+++ src/components/Admin/UserView/UserView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserView(props) {
+    return (
+        <div>
+            <h1>유저 상세 조회 페이지 입니다</h1>
+        </div>
+    );
+}
+
+export default UserView;
