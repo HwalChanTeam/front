89d5dd0a513d181f93739f5989053d27fbc55d37
qwdltjdals <iu_love@kakao.com>
2024-10-08 15:50:40 +0900

    상품 추가 페이지 정리

--- src/components/Admin/ProductRegister/style.js
+++ src/components/Admin/ProductRegister/style.js
@@ -10,18 +10,28 @@ export const mainBox = css`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-bottom: 350px;
+    margin-bottom: 100px;
     border: 1px solid #000000;
     height: 70px;
     width: 500px;
   }
 `;
 
+export const imgBox = css`
+  & img {
+    width: 250px;
+    height: 250px;
+    border: 1px solid #000000;
+    margin-bottom: 100px;
+  }
+`;
+
 export const inputBox = css`
   font-size: 18px;
-  width: 100%;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  width: 330px;
   & label {
     padding-right: 7px;
     padding-left: 7px;
@@ -35,17 +45,33 @@ export const inputBox = css`
     border-radius: 5px;
     padding: 0px 10px;
   }
-  & button {
-    font-size: 20px;
-    border: 1px solid #000000;
-    background-color: white;
-    border-radius: 5px;
-    margin-left: 7px;
-    &:hover {
-        background-color: #efefef99;
-    }
-    &:active {
-        background-color: #aaaaaa99;
+  & span {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    :nth-last-of-type(1) {
+      margin-bottom: 0;
     }
   }
+  & :nth-of-type(1) {
+    display: flex;
+    justify-content: left;
+  }
+`;
+
+export const buttonBox = css`
+    & button {
+  font-size: 20px;
+  border: 1px solid #000000;
+  background-color: white;
+  border-radius: 5px;
+  margin-top: 40px;
+  margin-left: 7px;
+  &:hover {
+      background-color: #efefef99;
+  }
+  &:active {
+      background-color: #aaaaaa99;
+  }
+}
 `;
