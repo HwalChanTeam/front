34d3d4417f826308ea9ebf92f5cda80f41bdc89f
qwdltjdals <iu_love@kakao.com>
2024-10-10 15:15:20 +0900

    조회

---
+++ src/components/Admin/ProductView/style.js
@@ -0,0 +1,94 @@
+import { css } from "@emotion/react";
+
+export const mainBox = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  & > h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 100px;
+    border: 1px solid #000000;
+    height: 70px;
+    width: 500px;
+  }
+`;
+
+export const tableBox = css`
+    box-sizing: border-box;
+    width: 80%;
+    height: 500px;
+    overflow-y: scroll;
+    border: 1px solid #000000;
+`;
+
+export const table = css`
+    box-sizing: border-box;
+    padding: 20px;
+    overflow-y: hidden;
+    width: 100%;
+    height: 500px;
+    & p {
+        display: flex;
+        justify-content: center;
+    }
+`;
+
+export const productBox = css`
+  box-sizing: border-box;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 100%;
+    height: 75px;
+    margin-bottom: 10px;
+
+
+  }
+`;
+
+export const tableDiv = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0 20px;
+`;
+
+export const spanBox = css`
+    display: flex;
+    flex-grow: 1; /* 추가: 남는 공간을 차지하도록 설정 */
+    padding-left: 40px;
+    & span {
+        font-size: 20px;
+        font-weight: 600;
+        margin-right: 30px;
+    }
+`;
+
+export const buttonBox = css`
+    display: flex;
+    & button {
+      font-size: 20px;
+      border: 1px solid #000000;
+      background-color: white;
+      border-radius: 5px;
+      margin-left: 7px;
+      &:hover {
+        background-color: #efefef99;
+      }
+      &:active {
+        background-color: #aaaaaa99;
+      }
+    }
+`;
--- src/components/Admin/StaffManagement/StaffManagement.jsx
+++ src/components/Admin/StaffManagement/StaffManagement.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function StaffManagement(props) {
-    return (
-        <div>
-            <h1>직원 관리 페이지 입니다.</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>직원 관리 페이지 입니다.</h1>
+      {/* 
+                1. 직원 회원가입? 페이지
+                */}
+    </div>
+  );
 }
 
 export default StaffManagement;
