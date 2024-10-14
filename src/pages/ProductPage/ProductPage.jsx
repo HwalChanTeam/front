7f1594a7d7ca375af85c7a8587d17bc07e95abcc
younghee05 <k1437013@gmail.com>
2024-10-14 11:21:08 +0900

    상품페이지 디자인 추가(미완)

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,53 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import MainMenu from '../../components/MainMenu/MainMenu';
+import { FiShoppingCart } from "react-icons/fi";
 
 function ProductPage(props) {
     return (
         <div css={s.layout}>
             <MainMenu />
-            
+            <div css={s.productLayout}>
+                <div css={s.imgLayout}>
+                    <img src="https://semie.cooking/image/contents/recipe/ee/hy/xdlvlsdq/131722691qqag.jpg" />
+                </div>
+                <div css={s.productContent}>
+                    <div css={s.titleLayout}>
+                        <h2>부대찌개</h2>
+                        <p>부대찌개 설명</p>
+                    </div>
+                    <div>
+                        11,000
+                    </div>
+                    <div css={s.contentBox}>
+                        <div>
+                            <p>원산지: 대한민국</p>
+                            <p>배송구분: 일반배송</p>
+                            <p>배송비: 3000원</p>
+                        </div>
+                        <div>
+                            <p>상품명: 부대찌개</p>
+                            <p>
+                                구매수량: 
+                                    <span>
+                                        <button>+</button>
+                                                1
+                                        <button>-</button>
+                                    </span>
+                            </p> 
+                        </div>
+                    </div>
+                    <div>
+                        <p>총 상품 금액: 14,000원
+                            <span>
+                                <button>구매하기</button>
+                                <FiShoppingCart />
+                            </span>
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/ProductPage/style.js
+++ src/pages/ProductPage/style.js
@@ -2,6 +2,51 @@ import { css } from "@emotion/react";
 
 export const layout = css`
     box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     margin-top: 40px;
     width: 100%;
+`;
+
+export const productLayout = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 40px;
+    width: 100%;
+    height: 100%;
+
+`;
+
+export const imgLayout = css`   
+    box-sizing: border-box;
+    width: 500px;
+    height: 500px;
+    
+    & > img {
+        width: 100%;
+        height: 100%;
+    };
+`;
+
+export const productContent = css`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    padding-left: 30px;
+    width: 100%;
+    height: 100%;
+`;
+
+export const titleLayout = css`
+    & h2 {
+        margin: 0px;
+    }
+`;
+
+export const contentBox = css`
+    box-sizing: border-box;
+    display: flex;
 `;
