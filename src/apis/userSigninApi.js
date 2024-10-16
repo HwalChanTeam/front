57922b588d0b6d7d2df433b9c342dcf48bfc2e41
younghee05 <k1437013@gmail.com>
2024-10-16 17:48:29 +0900

    user login api create

---
+++ src/apis/userSigninApi.js
@@ -0,0 +1,41 @@
+import { instance } from "./util/instance"
+
+export const userSigninApi = async(user) => {
+    let userSiginData = {
+        isSuccess: false,
+        token: null,
+        successMessage: "",
+        fieldErrors: [
+            {
+                field: "",
+                defaultMessage: ""
+            }
+        ]
+    } 
+    try {
+        const response = await instance.post("/user/signin", user);
+        userSiginData = {
+            isSuccess: true,
+            token: response.data,
+            successMessage: "로그인 성공!!"
+        }
+    } catch (error) {
+        const response = error.response;
+        userSiginData ={
+            isSuccess: false
+        };
+
+        if(response.status === 403) {
+            userSiginData['errorStatus'] = 'loginError';
+            userSiginData['error'] = response.data;
+        };
+
+        if(typeof(response.data) === 'string') {
+            userSiginData['errorStatus'] = 'loginError';
+            userSiginData['error'] = response.data;
+        }
+
+
+    }
+
+}
