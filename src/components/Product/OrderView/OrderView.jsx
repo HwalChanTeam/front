edcd6bd0c3fe903a746c668fe33c46678d9c337f
qwdltjdals <iu_love@kakao.com>
2024-10-16 10:50:14 +0900

    장바구니 페이지 삭제, 주문, 확인 기능 추가

---
+++ src/components/Product/OrderView/OrderView.jsx
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import * as s from './style';
+
+function OrderView(props) {
+
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default OrderView;
+
+// 상품 상세페이지 / 장바구니 페이지에서 주문하기 버튼 누르면 정보 가지고 들어오기
+// 갯수 추가는 필요 X
+// 주소 입력
+// 결제
