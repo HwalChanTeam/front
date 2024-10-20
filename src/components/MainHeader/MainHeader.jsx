82a67adb5cdafd3122dc724ac294ab9ccca6fe95
Skeat <iu_love@kakao.com>
2024-10-20 14:47:08 +0900

    로그아웃 기능 추가, 상태에 따라 렌더링 되게 수정

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from "react";
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import { Link, useNavigate } from "react-router-dom";
-import { BsList } from "react-icons/bs";
-import MainSearch from "../MainSearch/MainSearch";
-import { instance } from "../../apis/util/instance";
 import logo from "../../assets/images/logo.png";
-import { RiLoginBoxLine } from "react-icons/ri";
+import { RiLoginBoxLine, RiLogoutBoxRLine } from "react-icons/ri";
 import { LuUser } from "react-icons/lu";
 import { SlBasket } from "react-icons/sl";
-import { MdOutlineLocalShipping } from "react-icons/md";
 
 
 function MainHeader(props) {
-  const navigate = useNavigate();
-  
-  
+  const token = localStorage.getItem("accessToken");
+  const navigate = useNavigate(); // useNavigate 훅을 사용합니다.
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken"); // 로컬 스토리지에서 토큰 삭제
+    navigate("/");
+    window.location.reload(); // 페이지를 새로 고침하여 상태를 초기화
+  };
 
   return (
     <div css={s.background}>
@@ -26,12 +27,20 @@ function MainHeader(props) {
             Cuisson
           </h1>
         </Link>
-        {/* 로그인이 되어 있지 않으면 아래꺼 랜더링 */}
-        <div css={s.buttonLayout}>
+        {
+          !token ?
+          <div css={s.buttonLayout}>
           <Link to={"/user/signin"}><RiLoginBoxLine /></Link>
           <Link to={"/basket"}><SlBasket /></Link>
           <Link to={"/mypage"}><LuUser /></Link>
         </div>
+        :
+        <div css={s.buttonLayout}>
+        <Link onClick={handleLogout}><RiLogoutBoxRLine /></Link>
+        <Link to={"/basket"}><SlBasket /></Link>
+        <Link to={"/mypage"}><LuUser /></Link>
+      </div>
+          }
       </div>
     </div>
   );
