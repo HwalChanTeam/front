882f31e9e2556ad359e29653a2fe0f466e55d9cf
younghee05 <k1437013@gmail.com>
2024-10-21 16:25:59 +0900

    메인페이지 디자인 추가

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -18,22 +18,128 @@ function MainPage() {
                     <p>NEW PRODUCT</p>
                     <p>이 달의 신상품</p>
                 </div>
-                <div css={s.newProductImgLayout}>
-                    <div>
-                        <img src="" />
+                <div>
+
+                </div>
+                <div css={s.newProductContentLayout}>
+                    <div css={s.imgLayout}>
+                        <img src="https://semie.cooking/image/contents/recipe/ee/hy/xdlvlsdq/131722691qqag.jpg" />
+                        <div>
+                            <p>부대찌개</p>
+                            <p>11,000 원</p>
+                        </div>
                     </div>
-                    <div>
+                    <div css={s.imgLayout}>
                         <img src="" />
+                        <div>
+                            <p>fffff</p>
+                            <p>11,000 원</p>
+                        </div>
                     </div>
-                    <div>
+                    <div css={s.imgLayout}>
                         <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.imgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            {/* 베스트 상품top4 */}
+            {/* 베스트 상품top5 */}
+            <div css={s.bestProductLayout} > 
+                <div css={s.bestProductTitle}>
+                    <p>BEST PRODUCT</p>
+                    <p>베스트 상품 TOP5</p>
+                </div>
+                <div css={s.bestProductContentLayout}>
+                    <div css={s.bestImgLayout}>
+                        <img src="https://semie.cooking/image/contents/recipe/ee/hy/xdlvlsdq/131722691qqag.jpg" />
+                        <div>
+                            <p>부대찌개</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.bestImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>fffff</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.bestImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.bestImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.bestImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            {/* MD 추천상품(4개) */}
-
+            {/* MD 추천상품(5개) */}
+            <div css={s.MDProductLayout} > 
+                <div css={s.MDProductTitle}>
+                    <p>MD 추천상품</p>
+                </div>
+                <div css={s.MDProductContentLayout}>
+                    <div css={s.MDProductImgLayout}>
+                        <img src="https://semie.cooking/image/contents/recipe/ee/hy/xdlvlsdq/131722691qqag.jpg" />
+                        <div>
+                            <p>부대찌개</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.MDProductImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>fffff</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.MDProductImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.MDProductImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                    <div css={s.MDProductImgLayout}>
+                        <img src="" />
+                        <div>
+                            <p>gggg</p>
+                            <p>11,000 원</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         
         </>
     );
