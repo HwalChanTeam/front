d1f163131e4d8b4028388c734bb595e2bb3b3d26
Skeat <iu_love@kakao.com>
2024-10-23 19:43:21 +0900

    왜되지?

--- src/apis/util/atom.js
+++ src/apis/util/atom.js
@@ -4,8 +4,3 @@ export const selectedItemsAtom = atom({
     key: 'selectedItemsAtom', // 유일한 키
     default: [], // 기본값
   });
-
-  export const userIdAtom = atom({
-    key: 'userIdAtom',
-    default: null, // 로그인 시 userId가 저장될 것
-  });
