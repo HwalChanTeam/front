92e5ad666518e2a7932d0a5635e8078970826db0
qwdltjdals <iu_love@kakao.com>
2024-10-24 09:42:26 +0900

    페이지 내용 삭제

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -9,74 +9,6 @@ import { FcGoogle } from "react-icons/fc";
 import { sendEmailApi } from "../../apis/emailApi";
 
 function SignupPage(props) {
-  const navigate = useNavigate();
-
-  const [certification, setCertification] = useState(0);
-
-  const [user, setUser] = useState({
-    username: "",
-    password: "",
-    checkPassword: "",
-    name: "",
-    email: "",
-    phone: "",
-  });
-
-  const [sendEmail, setSendEmail] = useState({
-    toEmail: "",
-    subject: "",
-    content: "",
-    checkEmail: "",
-  });
-
-  const handleInputOnchange = (e) => {
-    setUser((user) => ({
-      ...user,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleEmailInputOnchange = (e) => {
-    setUser((user) => ({
-      ...user,
-      [e.target.name]: e.target.value,
-    }));
-    setSendEmail((email) => ({
-      ...email,
-      toEmail: user.email,
-    }));
-  };
-
-  const emailSendButtonOnClick = async () => {
-    console.log(sendEmail);
-    const emailData = await sendEmailApi(sendEmail);
-    if (certification == 1) {
-      alert("이미 인증 요청이 되었습니다.");
-      return;
-    }
-    setCertification(1);
-  };
-
-  const emailCheckOnChange = (e) => {
-    setUser((user) => ({
-      ...user,
-
-    }))
-  }
-
-  const emailCkeckButtonOnClick = async () => {
-    console.log(sendEmail);
-    const emailData = await sendEmailApi(sendEmail);
-    if (certification == 1) {
-      alert("이미 인증 요청이 되었습니다.");
-      return;
-    }
-    setCertification(1);
-  };
-
-  const handleSubmitButtonOnClick = () => {
-    // 백엔드로 요청 들어가야함(post요청)
-  };
 
   return (
     <div css={s.mainLayout}>
@@ -90,37 +22,27 @@ function SignupPage(props) {
         <div css={s.inputUser}>
           <input
             type="text"
-            onChange={handleInputOnchange}
             name="username"
-            value={user.username}
             placeholder="아이디를 입력해 주세요"
           />
           <input
             type="text"
-            onChange={handleInputOnchange}
             name="password"
-            value={user.password}
             placeholder="비밀번호를 입력해 주세요"
           />
           <input
             type="text"
-            onChange={handleInputOnchange}
             name="checkPassword"
-            value={user.checkPassword}
             placeholder="비밀번호를 한번 더 입력해 주세요"
           />
           <input
             type="text"
-            onChange={handleInputOnchange}
             name="name"
-            value={user.name}
             placeholder="이름을 작성해 주세요"
           />
           <input
             type="text"
-            onChange={handleInputOnchange}
             name="phone"
-            value={user.phone}
             placeholder="휴대폰 번호를 입력해 주세요"
           />
         </div>
@@ -128,14 +50,12 @@ function SignupPage(props) {
           <input
             type="text"
             name="email"
-            value={user.email}
             placeholder="이메일을 입력해 주세요"
-            onChange={handleEmailInputOnchange}
           />
-          <button onClick={emailSendButtonOnClick}>인증요청</button>
+          <button>인증요청</button>
 
           {/* 인증번호 요청 누르면 인증번호 칸 활성화되게(추가) */}
-          {certification === 0 ? (
+          {0 === 0 ? (
             <></>
           ) : (
             <div css={s.emailCkeck}>
@@ -143,15 +63,13 @@ function SignupPage(props) {
               type="text"
               neme="emailCheck"
               placeholder="인증번호를 입력해 주세요"
-              onChange={emailCheckOnChange}
-              value={user.checkEmail}
             />
-            <button onClick={() => handleEmailInputOnchange(sendEmail.checkEmail)}>확인</button>
+            <button>확인</button>
             </div>
           )}
         </div>
       <div css={s.joinOkButton}>
-        <button onClick={handleSubmitButtonOnClick}>회원가입 하기</button>
+        <button>회원가입 하기</button>
       </div>
       <div css={s.oauth2Buttons}>
           <button>
