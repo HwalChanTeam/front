31b2f5b10c026431f12daad746282b4a9f68e33e
younghee05 <k1437013@gmail.com>
2024-10-29 17:47:04 +0900

    검색 기능 추가(미완)

--- src/components/MainSearch/MainSearch.jsx
+++ src/components/MainSearch/MainSearch.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import { IoSearchSharp } from "react-icons/io5";
+import { useNavigate, useParams } from "react-router";
+import { useQuery } from "react-query";
+import { instance } from "../../apis/util/instance";
+import { useSearchParams } from "react-router-dom";
 
 function MainSearch({ onSearch }) {
+
+    const navigate = useNavigate();
+
+    const [ searchParam, setSearchParam ] = useSearchParams(); // 검색 param 상태
+
+    // 검색 상태
     const [mainSearchProduct, setMainSearchProduct] = useState({
         title: "",
     });
+
+    // 상품데이터 제한 수
+    const limit = 20;
+
+    // 검색 query
+    const mainSearch = useQuery(
+        ["mainSearch", ],
+        // page: 해당 페이지 불러오고 limit: 20 제한 search: 검색한 데이터 
+        async() => await instance.get(`/user/public/product/search?page=${searchParam.get("page")}&limit=${limit}&search=${mainSearchProduct}`), 
+        {
+            retry: 0,
+            refetchOnWindowFocus: false,
+            onSuccess: {
+                
+            }
+        }
+    );
 
     const handleMainSearchOnChange = (e) => {
         setMainSearchProduct((mainSearchProduct) => ({
@@ -26,6 +53,7 @@ function MainSearch({ onSearch }) {
             setMainSearchProduct({
                 title: "",
             });
+            navigate(`/product/`)
         }
     }
 
