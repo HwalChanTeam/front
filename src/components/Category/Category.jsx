0b85dc02b16da7695b682a6ca7d014a61800740d
younghee05 <k1437013@gmail.com>
2024-11-02 17:52:05 +0900

    카테고리 맵 수정 및 디자인 수정

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -13,7 +13,7 @@ function Category(props) {
     const productPath = (productId) => `/product/${productId}`; // 해당 상품의 상품페이지로 이동할려고 만든 productId
     const [searchParams, setSearchParams] = useSearchParams(); // 주소:포트/페이지URL?key=value(쿼리스트링, 파람스)
     // const categoryId = useParams();
-    const [productList, setProductList] = useState();
+    const [productList, setProductList] = useState([]);
     const categoryId = searchParams.get("categoryId");
     const [pageCount, setPageCount] = useState(1);
     const limit = 20;
@@ -23,23 +23,44 @@ function Category(props) {
     //     limit: 20,
     //     categoryId: categoryId.categoryId,
     // })
-    // 냉장 조회 query
-    const category = useQuery(
 
+    // 카테고리 조회 query
+    const category = useQuery(
         ["category", categoryId, pageCount],
         async () => {
             return await instance.get(`/user/public/product/category?categoryId=${categoryId}&page=${pageCount}&limit=${limit}`); // 추후 수정 예정 
         },
         {
-
             refetchOnWindowFocus: false,
-            retry: 0,
-            onSuccess: response => setPageCount(
-                response.data.count % limit === 0
-                    ? response.data.count / limit
-                    : Math.floor(response.data.count / limit) + 1)
+            retry: 0,   
+            onSuccess: response => {
+                // 임시로 productList 사용 추후에 수정예정 ( 상태를 뭐로 할지 )
+                const responseData = response.data // td의 배열 길이
+                let newProducts = responseData.length
+                console.log(response.data)
+                let result = []
+                // 배열을 5개씩으로 분류 하도록 하고 결과값이 소수가 나올경우 정수로 변환시켜주기 위해 floor 사용함  
+                // productList % 5를 했을 때 나머지가 0이면 그대로 몫울 1로 두고 아니면 그 몫에 1을 더해라 
+                let num =  Math.floor(newProducts % 5 === 0 ?  newProducts / 5 : (newProducts / 5) + 1)  // 나머지 존재, 2까지 반복 / num = 2
+                for(let i = 0; i < num; i++) {
+                    let newProductArray = []
+                    // 1. j < 2 - 1 === 0 
+                    for(let j = 0; j < (num - 1 === i ? (newProducts % 5 === 0 ? 5 : newProducts % 5) : 5); j++ ) {
+                        newProductArray[j] = responseData[j + (i * 5)]
+                    }
+                    result[i] = newProductArray
+                }
+                setProductList(result)
+                // page 수  
+                setPageCount(
+                    response.data.count % limit === 0
+                        ? response.data.count / limit
+                        : Math.floor(response.data.count / limit) + 1)
+            }
+                
         }
     );
+    console.log(category);
 
     const handleOnPageChange = (e) => {
         navigate(`/user/category?categoryId=${categoryId}&page=${e.selected + 1}&limit=${limit}`); 
@@ -48,33 +69,32 @@ function Category(props) {
     return (
         <div css={s.layout}>
             <div css={s.contentLayout}>
-                <table css={s.tableLayout}>
-                    <tbody css={s.menuLayout}>
-
-                        {
-                            category?.data?.data?.products?.map((product) => (
-
-                                <tr>
+                {
+                    category?.data?.data?.products?.map((product) => (
+                        <tr css={s.menuLayout}>
+                            {
+                                product.map((pro) => ( 
                                     <td>
                                         <div css={s.menuList}>
                                             <div css={s.imgLayout}>
                                                 <Link
-                                                    key={product.id}
-                                                    to={productPath(product.productId)}>
-                                                    <img src={product.thumbnailImg} />
+                                                    key={pro.id}
+                                                    to={productPath(pro.productId)}>
+                                                    <img src={pro.thumbnailImg} />
                                                 </Link>
                                             </div>
                                             <div css={s.productLayout}>
-                                                <h2>{product.title}</h2>
-                                                <h2>{product.price}</h2>
+                                                <p>{pro.description}</p>
+                                                <h2>{pro.title}</h2>
+                                                <h2>{pro.price}</h2>
                                             </div>
                                         </div>
                                     </td>
-                                </tr>
-                            ))
-                        } 
-                    </tbody>
-                </table>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
             </div>
             <div css={s.pageNumber}>
                 <ReactPaginate
