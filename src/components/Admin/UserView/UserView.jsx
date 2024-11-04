749ee4bbfe92c77124453ce0b24600dec54f6f77
younghee05 <k1437013@gmail.com>
2024-11-04 14:32:46 +0900

    직원관리, 유저관리 디자인 수정

--- src/components/Admin/UserView/UserView.jsx
+++ src/components/Admin/UserView/UserView.jsx
@@ -1,10 +1,58 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from 'react';
 import * as s from './style';
+import { useMutation, useQuery } from 'react-query';
+import { instance } from '../../../apis/util/instance';
+import Modal from '../../Modal/Modal';
 
 function UserView(props) {
+    // 모달 띄우는 상태 추가
+    const [openModal, setOpenModal] = useState(false);
+    const [checkedIds, setCheckedIds] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    const closeModal = () => {
+        setOpenModal(false); // 모달 닫기
+    };
+
+    const userQuery = useQuery(
+        ["useQuery"],
+        async () => {
+            const response = await instance.get("admin/user?role=3");
+            console.log(response?.data?.user);
+            setUsers(response?.data?.user);
+        }
+    );
+
+    const deleteMutation = useMutation(
+        async () => {
+            console.log(checkedIds);
+            await instance.delete("/admin/user", { data: { checkedIds } });
+        },
+        {
+            retry: 0,
+            refetchOnWindowFocus: false,
+            onSuccess: (response) => {
+                alert("삭제가 완료되었습니다.");
+                userQuery.refetch();
+            }
+        }
+    );
+    
+    const handleCheckBoxOnChange = (userId) => {
+        console.log(userId);
+        setCheckedIds((ids) => {
+            if (ids.includes(userId)) {
+                return ids.filter(id => id !== userId);
+            } else {
+                return [...ids, userId];
+            }
+        });
+    };
+
     return (
         <div css={s.mainBox}>
-            <h1>유저 조회</h1>
+            <h1>유저 관리</h1>
             {/* 가져와야 할거
                 아이디
                 이름 등 개인정보
@@ -13,6 +61,45 @@ function UserView(props) {
                 구매목록
                 배송현황??(옵션)
             */}
+            <div css={s.buttonLayout}>
+                <button onClick={() => setOpenModal(true)}>등록</button>
+                <Modal isOpen={openModal} onClose={closeModal} />
+                <button onClick={() => deleteMutation.mutateAsync()}>삭제</button>
+            </div>
+            <div css={s.container}>
+                <table css={s.theadLayout}>
+                    <tr>
+                        <td css={s.theadItems}>선택</td>
+                        <td css={s.theadItems}>번호</td>
+                        <td css={s.theadItems}>이름</td>
+                        <td css={s.theadItems}>아이디</td>
+                        <td css={s.theadItems}>이메일</td>
+                        <td css={s.theadItems}>휴대폰</td>
+                        <td css={s.theadItems}>생성일자</td>
+                    </tr>
+                </table>
+                <table css={s.tableLayout}>
+                    {/* <tbody css={s.tbodyLayout}> */}
+                    {users.map((user) => (
+                        <tr key={user.userId}>
+                            <td css={s.productItem}>
+                                <input
+                                    type="checkbox"
+                                    onChange={() => handleCheckBoxOnChange(user.userId)}
+                                    checked={checkedIds.includes(user.userId)}
+                                />
+                            </td>
+                            <td css={s.productItem}>{user.userId}</td>
+                            <td css={s.productItem}>{user.name}</td>
+                            <td css={s.productItem}>{user.username}</td>
+                            <td css={s.productItem}>{user.email}</td>
+                            <td css={s.productItem}>{user.phoneNumber}</td>
+                            <td css={s.productItem}>{user.createdAt}</td>
+                        </tr>
+                    ))}
+                    {/* </tbody> */}
+                </table>
+            </div>
         </div>
     );
 }
--- src/components/Admin/UserView/style.js
+++ src/components/Admin/UserView/style.js
@@ -14,3 +14,145 @@ export const mainBox = css`
     color: #ffffff;
   }
 `;
+
+export const buttonLayout = css`
+  box-sizing: border-box;
+  padding: 90px 0 50px 20px;
+  width: 300px;
+  height: 50px;
+
+  & button {
+    box-sizing: border-box;
+    width: 50px;
+    height: 30px;
+    background-color: #3a3a3a;
+    border: 1px solid #000000;
+    color: #f5f5f5;
+    font-size: 14px;
+    font-weight: 600;
+
+    &:hover {
+      background-color: #686d76;
+    }
+    &:active {
+      background-color: #3d3b40;
+    }
+
+    &:nth-last-of-type(1) {
+      margin-left: 20px;
+    }
+  }
+`;
+
+export const container = css`
+  box-sizing: border-box;
+  margin: 0px 20px;
+  border: 1px solid #000000;
+  width: 1000px;
+  height: 500px;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const theadLayout = css`
+  box-sizing: border-box;
+  border-collapse: collapse;
+  padding: 0px;
+  width: 100%;
+  height: 22px;
+  position: sticky; /* 스티키 포지션 적용 */
+  top: 0; /* 상단 고정 */
+  background-color: #fff; /* 배경색 추가 */
+  /* z-index: 1; 다른 요소 위에 있도록 설정 */
+
+`;
+
+export const theadItems = css`
+  box-sizing: border-box;
+  padding: 0px;
+  border-right: 1px solid #000000;
+  cursor: default;
+
+  &:nth-of-type(1) { 
+    text-align: center;
+    width: 40px;
+  }
+
+  &:nth-of-type(2) {
+    width: 50px;
+  }
+
+  &:nth-of-type(3) {
+    width: 120px;
+  }
+
+  &:nth-of-type(4) {
+    width: 140px;
+  }
+
+  &:nth-of-type(5) {
+    width: 285px;
+  }
+
+  &:nth-of-type(6) {
+    width: 160px;
+  }
+
+  &:nth-of-type(7) {
+    width: 203px;
+    border-right: none;
+  }
+`;
+
+export const tableLayout = css`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+export const productItem = css`
+  box-sizing: border-box;
+  border: 1px solid #000000;
+  border-left: none;
+  padding: 0px;
+  padding-left: 5px;
+  cursor: default;
+
+  &:nth-of-type(1) {
+    padding: 0px;
+    text-align: center;
+    width: 40px;
+  }
+
+  &:nth-of-type(2) {
+    width: 50px;
+  }
+
+  &:nth-of-type(3) {
+    width: 120px;
+  }
+
+  &:nth-of-type(4) {
+    width: 140px;
+  }
+
+  &:nth-of-type(5) {
+    width: 285px;
+  }
+
+  &:nth-of-type(6) {
+    width: 160px;
+  }
+
+  &:nth-of-type(7) {
+    padding: 0px;
+    padding-right: 5px;
+    width: 203px;
+    text-align: right;
+    border-right: none;
+  }
+`;
+
+
