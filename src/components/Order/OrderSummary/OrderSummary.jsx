d281289dab7026b4edd120e97d15195583a8f2ed
kim0705 <dbwls138@naver.com>
2024-11-11 02:54:35 +0900

    결제 취소 로직 구현 중

--- src/components/Order/OrderSummary/OrderSummary.jsx
+++ src/components/Order/OrderSummary/OrderSummary.jsx
@@ -9,7 +9,7 @@ import Swal from "sweetalert2";
 import { channelKey } from "../../../portone/portone";
 
 // 오른쪽 결제 박스
-function OrderSummary({ productList, payMentState, userInfo, totalAmount, buyButtonOnClick, deliveryFee, totalProductAmount }) {
+function OrderSummary({ productList, payMentState, userInfo, totalAmount, deliveryFee, totalProductAmount }) {
     const [products, setProducts] = useState([]);
     const navigate = useNavigate();
 
--- src/components/Order/PaymentMethod/PaymentMethod.jsx
+++ src/components/Order/PaymentMethod/PaymentMethod.jsx
@@ -8,28 +8,17 @@ function PaymentMethod({ setPayMentState }) {
     // 클릭하였을 때 빨간 테두리 뜨게할려고 만든 상태
     // 신용카드
     const [ cardRedOutLine, setCardRedOutLine ] = useState(false);
-    // 무통장
-    const [ bankAccountRedOutLine, setBankAccountRedOutLine ] = useState(false);
 
     const handleCardClick = () => {
         setPayMentState("CARD");
         setCardRedOutLine(true); // 카드의 상태가 true로 바뀌면서 테두리 o
-        setBankAccountRedOutLine(false); // 무통장의 상태가 false로 바뀌면서 테두리 x
     };
-
-    // const handleBankAccountClick = () => {
-    //     setPatMentState("bankbook");
-    //     setCardRedOutLine(false); // 카드의 상태가 false로 바뀌면서 테두리 x
-    //     setBankAccountRedOutLine(true); // 무통장의 상태가 true로 바뀌면서 테두리 o
-    // };
 
     return (
         <div css={s.payInfo}>
             <h2>결제수단 선택</h2>
             <button css={s.selectClick(cardRedOutLine)} 
                 onClick={handleCardClick}>신용카드</button>
-            {/* <button css={s.selectClick(bankAccountRedOutLine)} 
-                onClick={handleBankAccountClick}>무통장 입금</button> */}
         </div>
     );
 }
